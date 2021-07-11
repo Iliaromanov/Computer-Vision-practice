9d252f256f244c5fefb2b184e9e26ff207388490
Iliaromanov <iliaromanov033@gmail.com>
2021-07-11 15:45:54 -0400

    displaying face detection box and confidence score in output video

--- OpenCV-work/face_detection_intro.py
+++ OpenCV-work/face_detection_intro.py
@@ -11,13 +11,20 @@ mpDraw = mp.solutions.drawing_utils
 
 while True:
     success, img = cap.read()
+    h, w, c = img.shape
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = face_detection.process(img_rgb)
 
     detections = results.detections
 
     if detections:
-        for detection in detections:
+        for id, detection in enumerate(detections):
+            score = detection.score[0]
+            bounding_box = detection.location_data.relative_bounding_box
+            x, y = int(bounding_box.xmin * w), int(bounding_box.ymin * h)
+
+            cv2.putText(img, str(int(float(score)*100))+'%', (x, y-10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             mpDraw.draw_detection(img, detection)
 
     cv2.imshow("Image", img)
