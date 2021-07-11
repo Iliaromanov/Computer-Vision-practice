2d17fdd099475589b181c611d5cce18104594432
Iliaromanov <iliaromanov033@gmail.com>
2021-07-10 23:22:30 -0400

    creating hand detection module

---
+++ OpenCV-work/HandTrackingModule.py
@@ -0,0 +1,61 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+class HandDetector:
+    def __init__(self, mode=False, max_num_hands=2,
+                 min_detection_confidence=0.5, min_tracking_confidence=0.5):
+        self.mode = mode
+        self.max_num_hands = max_num_hands
+        self.min_detection_confidence = min_detection_confidence
+        self.min_tracking_confidence = min_tracking_confidence
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.max_num_hands,
+                                        self.min_detection_confidence, self.min_tracking_confidence)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def find_hands(self, img, draw=True):
+        height, width, channels = img.shape # get frames dimensions and channels
+
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Convert frame img to RGB
+        results = self.hands.process(img_rgb)
+
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, hand_landmarks, self.mpHands.HAND_CONNECTIONS)
+
+        return img
+        # for id, lm in enumerate(hand_landmarks):
+        #     x_pos, y_pos = int(lm.x * width), int(lm.y * height)
+
+
+def main():
+    # Create video object
+    capture = cv2.VideoCapture(0)  # using webcam number 0
+
+    detector = HandDetector()
+
+    p_time = 0
+    c_time = 0
+
+    while True:
+        success, img = capture.read()  # Get img frame
+        img = detector.find_hands(img)
+
+        # Calculate frame rate
+        c_time = time.time()
+        fps = 1 / (c_time - p_time)
+        p_time = c_time
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255),
+                    3)  # Display text on frame
+
+        cv2.imshow("Image", img)  # Display frame
+        cv2.waitKey(1)  # Display each frame for 1ms
+
+
+if __name__ == "__main__":
+    main()
